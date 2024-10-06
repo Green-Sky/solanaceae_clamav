65931a7ffaceb703c77b7fe9652c418ccc466c0d
Green Sky <green@g-s.xyz>
2024-10-06 12:26:13 +0200

    update utils and plugin

--- plugins/plugin_clamav_module.cpp
+++ plugins/plugin_clamav_module.cpp
@@ -4,17 +4,17 @@
 #include <solanaceae/util/config_model.hpp>
 
 #include <memory>
+#include <limits>
 #include <iostream>
 
-#define RESOLVE_INSTANCE(x) static_cast<x*>(solana_api->resolveInstance(#x))
-#define PROVIDE_INSTANCE(x, p, v) solana_api->provideInstance(#x, p, static_cast<x*>(v))
-
 static std::unique_ptr<ClamAVModule> g_cavm = nullptr;
+
+constexpr const char* plugin_name = "ClamAVModule";
 
 extern "C" {
 
 SOLANA_PLUGIN_EXPORT const char* solana_plugin_get_name(void) {
-	return "ClamAVModule";
+	return plugin_name;
 }
 
 SOLANA_PLUGIN_EXPORT uint32_t solana_plugin_get_version(void) {
@@ -22,43 +22,40 @@ SOLANA_PLUGIN_EXPORT uint32_t solana_plugin_get_version(void) {
 }
 
 SOLANA_PLUGIN_EXPORT uint32_t solana_plugin_start(struct SolanaAPI* solana_api) {
-	std::cout << "PLUGIN CAVM START()\n";
+	std::cout << "PLUGIN " << plugin_name << " START()\n";
 
 	if (solana_api == nullptr) {
 		return 1;
 	}
 
-	ConfigModelI* conf = nullptr;
+	try {
+		auto* conf = PLUG_RESOLVE_INSTANCE(ConfigModelI);
 
-	{ // make sure required types are loaded
-		conf = RESOLVE_INSTANCE(ConfigModelI);
+		// static store, could be anywhere tho
+		// construct with fetched dependencies
+		g_cavm = std::make_unique<ClamAVModule>(*conf);
 
-		if (conf == nullptr) {
-			std::cerr << "PLUGIN CAVM missing ConfigModelI\n";
-			return 2;
-		}
+		// register types
+		PLUG_PROVIDE_INSTANCE(ClamAVModule, plugin_name, g_cavm.get());
+	} catch (const ResolveException& e) {
+		std::cerr << "PLUGIN " << plugin_name << " " << e.what << "\n";
+		return 2;
 	}
-
-	// static store, could be anywhere tho
-	// construct with fetched dependencies
-	g_cavm = std::make_unique<ClamAVModule>(*conf);
-
-	// register types
-	PROVIDE_INSTANCE(ClamAVModule, "ClamAVModule", g_cavm.get());
 
 	return 0;
 }
 
 SOLANA_PLUGIN_EXPORT void solana_plugin_stop(void) {
-	std::cout << "PLUGIN CAVM STOP()\n";
+	std::cout << "PLUGIN " << plugin_name << " STOP()\n";
 
 	g_cavm.reset();
 }
 
-SOLANA_PLUGIN_EXPORT void solana_plugin_tick(float delta) {
+SOLANA_PLUGIN_EXPORT float solana_plugin_tick(float delta) {
 	(void)delta;
 	//std::cout << "PLUGIN CAVM TICK()\n";
 	//g_cavm->iterate();
+	return std::numeric_limits<float>::max();
 }
 
 } // extern C
